822b598fc5b2f4a6ae9480b594f609d5c2918e2e
harshadachavan <harsh.chvn@gmail.com>
2017-05-30 11:08:35 +0530

    Type cast display ascii value of int

---
+++ AsciiValues.java
@@ -0,0 +1,15 @@
+package asciidemo;
+
+public class AsciiValues {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 65; i < 91;i++) {
+			
+			char j = (char)i;
+			System.out.println( i + " - "+j+" \n");
+		}
+
+	}
+
+}
